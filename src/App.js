b5cd19dbf96e00808242238cfd659eb44cf3f41a
xyz <00ram00singh@gmail.com>
2022-02-09 09:58:10 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import { Route, Routes } from 'react-router-dom';
+
+import AllMeetupsPage from './pages/AllMeetups';
+import NewMeetupPage from './pages/NewMeetup';
+import FavoritesPage from './pages/Favorites';
+import MainNavigation from './components/layout/MainNavigation';
+
+function App() {
+  return (
+    <div>
+      <MainNavigation />
+      <Routes>
+        <Route path='/' element={<AllMeetupsPage />}/>
+          
+        
+        <Route path='/new-meetup' element={<NewMeetupPage />}/>
+          
+        
+        <Route path='/favorites' element={<FavoritesPage />}/>
+      </Routes>
+    </div>
+  );
+}
+
+// export default App;
+
+export default App;
